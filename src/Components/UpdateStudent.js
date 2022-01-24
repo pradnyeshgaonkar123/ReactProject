cc56e09ea4eccb1cb5bcd919b84cf19c99669d42
pradnyeshgaonkar123 <pradnyeshgaonkar106@gmail.com>
2022-01-24 13:09:04 +0530

    made id input readonly

--- src/Components/UpdateStudent.js
+++ src/Components/UpdateStudent.js
@@ -27,7 +27,7 @@ export const UpdateStudent = () => {
 
     function searchStud(stId) {
         let found = studArr.find(s => s.studentId === parseInt(stId));
-        console.log("Found : "+found);
+        console.log("Found : " + found);
         return found;
     }
 
@@ -35,7 +35,7 @@ export const UpdateStudent = () => {
 
     let student = searchStud(id);
 
-    
+
 
     const [stud, setStudent] = useState(new Student(
         parseInt(student.studentId),
@@ -68,11 +68,11 @@ export const UpdateStudent = () => {
         dispatch(updateStudent(stud));
         e.preventDefault();
 
-       
+
         console.log("===============1============");
         console.log(studArr);
         console.log(stud);
-       
+
 
         console.log("===============2============");
         setFlag(false);
@@ -93,14 +93,14 @@ export const UpdateStudent = () => {
                         <Typography variant='caption' gutterBottom>Please fill this form to update student !</Typography>
                     </Grid>
                     <form onSubmit={submitForm}>
-                        <TextField fullWidth label='ID' placeholder="Enter your ID" onChange={(e)=>setStudent({...stud, ["studentName"]:e.target.value})} value={stud.studentId}  />
-                        <TextField fullWidth label='Name' placeholder="Enter your Name" value={stud.studeName} onChange={(e)=>setStudent({...stud, ["studeName"]:e.target.value})} />
+                        <TextField fullWidth label='ID' inputProps={{ readOnly: true, }} placeholder="Enter your ID" onChange={(e) => setStudent({ ...stud, ["studentName"]: e.target.value })} value={stud.studentId} />
+                        <TextField fullWidth label='Name' placeholder="Enter your Name" value={stud.studeName} onChange={(e) => setStudent({ ...stud, ["studeName"]: e.target.value })} />
 
-                        <TextField fullWidth label='Department' placeholder="Enter your department" value={stud.studDepart} onChange={(e)=>setStudent({...stud, ["studDepart"]:e.target.value})} />
-                        <TextField fullWidth label='Year' placeholder="Enter your Standard" onChange={(e)=>setStudent({...stud, ["studYear"]:e.target.value})} value={stud.studYear} />
+                        <TextField fullWidth label='Department' placeholder="Enter your department" value={stud.studDepart} onChange={(e) => setStudent({ ...stud, ["studDepart"]: e.target.value })} />
+                        <TextField fullWidth label='Year' placeholder="Enter your Standard" onChange={(e) => setStudent({ ...stud, ["studYear"]: e.target.value })} value={stud.studYear} />
 
                         <Button type='submit' style={btnStyle} variant='contained' color='primary'>UPDATE</Button>
-                       
+
                     </form>
                 </Paper>
             </Grid>
